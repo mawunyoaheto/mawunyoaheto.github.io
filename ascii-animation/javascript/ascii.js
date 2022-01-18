d63d60266e99f36fdb47bfc8f43c83c207d2d4f7
Raymond Mawunyo Aheto <40844791+mawunyoaheto@users.noreply.github.com>
2022-01-18 10:36:56 -0600

    implement animation

---
+++ ascii-animation/javascript/ascii.js
@@ -0,0 +1,93 @@
+"use strict";
+
+let currentAnimation;
+let startButton;
+let stopButton;
+let animation;
+let animationSelect;
+let animationSize;
+let chkBoxSpeed;
+var animating = false;
+var delay = 250;
+
+window.onload = () => {
+
+    startButton = document.getElementById("start");
+    startButton.onclick = startAnimation;
+    startButton.disabled=true;
+
+    stopButton = document.getElementById("stop");
+    stopButton.onclick = stopAnimation;
+    stopButton.disabled=true
+
+    animationSelect = document.getElementById("animations");
+    animationSelect.onchange = changeAnimation;
+
+    animationSize = document.getElementById("size");
+    animationSize.onchange = changeSize;
+
+    chkBoxSpeed = document.getElementById("speed");
+    chkBoxSpeed.onchange = onTurboChecked;
+}
+
+
+function startAnimation() {
+    startButton.disabled = true;
+    stopButton.disabled = false;
+    animationSelect.disabled = true;
+
+    if (currentAnimation) {
+        const animArray = ANIMATIONS[currentAnimation].split("=====\n");
+        let currentFrame = 0;
+
+        animation = setInterval(() => {
+            document.getElementById("txtAnimation").value = animArray[currentFrame];
+            currentFrame = (currentFrame === animArray.length - 1) ? 0 : currentFrame + 1;
+        }, delay);
+        animating = true;
+    }
+}
+
+function stopAnimation() {
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    animationSelect.disabled = false;
+    clearInterval(animation);
+    animating = false;
+}
+
+function changeAnimation() {
+    currentAnimation = animationSelect.value;
+    document.getElementById("txtAnimation").value = ANIMATIONS[currentAnimation].split("=====\n")[0];
+
+    if (currentAnimation.toLowerCase() !== "blank" && !animating)
+        startButton.disabled = false;
+
+    if (currentAnimation.toLowerCase() === "blank"){
+        startButton.disabled = true;
+        stopButton.disabled = true;
+    }
+        
+
+}
+
+function changeSize() {
+    makeSizer(animationSize.value)();
+    //resize 
+    function makeSizer(size) {
+        return function () {
+            document.getElementById("txtAnimation").style.fontSize = size;
+        };
+    }
+}
+
+const onTurboChecked = () => {
+    delay = chkBoxSpeed.checked ? 50 : 250;
+    if (animation) {
+        clearInterval(animation);
+        startAnimation();
+    }
+}
+
+
+
