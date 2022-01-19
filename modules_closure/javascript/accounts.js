972e2b09a3c96e991e62a298e7e571b5981b030c
Raymond Mawunyo Aheto <40844791+mawunyoaheto@users.noreply.github.com>
2022-01-19 14:51:14 -0600

    update to module implementation

--- modules_closure/javascript/accounts.js
+++ modules_closure/javascript/accounts.js
@@ -1,43 +1,53 @@
-window.onload=function(){
-    var btnCreateAccount=document.getElementById("create");
-    btnCreateAccount.onclick=onAddAccountClicked;
+window.onload = function () {
+    var btnCreateAccount = document.getElementById("create");
+    btnCreateAccount.onclick = onAddAccountClicked;
 };
 
-const accountInfoList = [];
-var newAccountModule = (function () {
-    var account = {
-        accountName: "",
-        deposit: 0
-    };
-    var createAccount=function(name, amount) {
-       
-        var newAccount = Object.create(account);
-        newAccount.accountName=name;
-        newAccount.deposit=amount;
-        return newAccount;
-    };
-    return {createAccount:createAccount};
-})();
+function onAddAccountClicked() {
+    let accountNameElement = document.getElementById("accout_name");
+    let depositeElement = document.getElementById("deposit");
+    const accountName = accountNameElement.value.trim();
+    const deposit = depositeElement.value.trim();
 
-function onAddAccountClicked(){
-    let accountNameElement=document.getElementById("accout_name");
-    let depositeElement=document.getElementById("deposit");
-    const accountName=accountNameElement.value.trim();
-    const deposit=depositeElement.value.trim();
-
-    if(accountName===""||deposit===""){
+    if (accountName === "" || deposit === "") {
         alert("Please enter account name and deposit");
         return;
     }
-    var newAcc=newAccountModule.createAccount(accountName,deposit);
-    accountInfoList.push(newAcc);
-    let mytextarea=document.getElementById("txtOutput");
-    mytextarea.value="";
-    accountNameElement.value="";
-    depositeElement.value="";
+    createAccount.createAccount(accountName, deposit);
+    let mytextarea = document.getElementById("txtOutput");
+    mytextarea.value = "";
+    accountNameElement.value = "";
+    depositeElement.value = "";
 
-    for(let i=0;i<accountInfoList.length;i++){
-        mytextarea.value +="Account name: "+accountInfoList[i].accountName +" Balance: "+accountInfoList[i].deposit+"\n";
-    }
-
+    mytextarea.value = createAccount.getDetails();
 }
+
+
+let createAccount = (function () {
+    let accountsInfoList = [];
+    let account = {
+        accountType: '',
+        amount: 0
+    };
+
+    let newAccount = (accType, depositAmount) => {
+        var acct = Object.create(account);
+        acct.accType = accType;
+        acct.depositAmount = depositAmount;
+        accountsInfoList.push(acct);
+        console.log(accountsInfoList);
+    };
+
+    let getAccountInfo = () => {
+        let details = "";
+
+        for (let obj of accountsInfoList)
+            details += "Account type: " + Object.values(obj)[0] + " " + "Balance: " + Object.values(obj)[1] + "\n";
+        return details;
+    };
+
+    return {
+        createAccount: newAccount,
+        getDetails: getAccountInfo
+    };
+})();
